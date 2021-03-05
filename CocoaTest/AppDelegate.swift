19ba29e4103042c135f2185f70f9b2ccfd0915fc
longx <longhaiqiang@me.com>
2021-03-05 21:08:29 +0800

    Initial Commit

---
+++ CocoaTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaTest
+//
+//  Created by 龙海强 on 2021/3/5.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CocoaTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CocoaTest
+//
+//  Created by 龙海强 on 2021/3/5.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
